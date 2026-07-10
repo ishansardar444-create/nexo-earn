4d970c2ab388a42cb0251595cd2ce6aebc5fd463
ishansardar444-create <ishansardar.444@gmail.com>
2026-07-11 02:30:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+const authRoutes = require("./routes/auth");
+
+app.use("/api/auth", authRoutes);
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
