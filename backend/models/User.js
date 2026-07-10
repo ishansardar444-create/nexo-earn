7f377dc737cc4a434373ed9f70e3a53deaa63377
ishansardar444-create <ishansardar.444@gmail.com>
2026-07-11 02:27:46 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,48 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema({
+  telegramId: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  userId: {
+    type: String,
+    unique: true
+  },
+  username: {
+    type: String
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  referralCode: {
+    type: String,
+    unique: true
+  },
+  referredBy: {
+    type: String,
+    default: null
+  },
+  balance: {
+    type: Number,
+    default: 0
+  },
+  totalEarned: {
+    type: Number,
+    default: 0
+  },
+  referrals: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
